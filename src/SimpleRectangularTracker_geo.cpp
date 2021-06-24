7da31993b18a7002fc47bb18970a461bda48d340
Alex Jentsch <ajentsch@bnl.gov>
2021-06-24 23:59:51 +0000

    Update src/SimpleRectangularTracker_geo.cpp

--- src/SimpleRectangularTracker_geo.cpp
+++ src/SimpleRectangularTracker_geo.cpp
@@ -69,7 +69,7 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
          s_vol.setSensitiveDetector(sens);
         }
         s_vol.setAttributes(description, x_slice.regionStr(), x_slice.limitsStr(), x_slice.visStr());
-        pv = m_vol.placeVolume(s_vol, Position(0, 0, z - zmin - layerWidth / 2 + thick / 2));
+        pv = l_vol.placeVolume(s_vol, Position(0, 0, z - zmin - layerWidth / 2 + thick / 2));
         pv.addPhysVolID("slice", s_num);
       }
 
