46a9ae8ba56c913f84265285bc76e0b56a5dd650
Whitney Armstrong <warmstrong@anl.gov>
2021-08-18 01:28:52 -0500

    modified:   CMakeLists.txt 	modified:   ip6/far_forward_offM_tracker.xml 	modified:   src/SimpleRectangularTracker_geo.cpp

--- src/SimpleRectangularTracker_geo.cpp
+++ src/SimpleRectangularTracker_geo.cpp
@@ -1,20 +1,3 @@
-//==========================================================================
-//  AIDA Detector description implementation 
-//--------------------------------------------------------------------------
-// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
-// All rights reserved.
-//
-// For the licensing terms see $DD4hepINSTALL/LICENSE.
-// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
-//
-// Author     : M.Frank
-// Modified   : W.Armstrong
-//
-//==========================================================================
-//
-// Specialized generic detector constructor
-// 
-//==========================================================================
 #include "DD4hep/DetFactoryHelper.h"
 
 using namespace std;
