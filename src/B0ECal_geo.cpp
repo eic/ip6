2226f8a21d1ea2429cd67a5cf10dfed105f2c39c
Sakib Rahman <rahmans@myumanitoba.ca>
2022-09-30 13:59:44 -0500

    Included function to build and place individual modules
    Copied the code to build and place individual modules from [here](https://github.com/eic/epic/blob/main/src/HomogeneousCalorimeter_geo.cpp#L196)

--- src/B0ECal_geo.cpp
+++ src/B0ECal_geo.cpp
@@ -85,4 +85,60 @@ static Ref_t createDetector(Detector& desc, xml_h e, SensitiveDetector sens)
   det.setPlacement(detPV);
   return det;
 }
+
+// helper function to build module with or w/o wrapper
+std::tuple<Volume, Position> build_module(Detector& desc, xml::Collection_t& plm, SensitiveDetector& sens)
+{
+  auto   mod = plm.child(_Unicode(module));
+  auto   sx  = mod.attr<double>(_Unicode(sizex));
+  auto   sy  = mod.attr<double>(_Unicode(sizey));
+  auto   sz  = mod.attr<double>(_Unicode(sizez));
+  Box    modShape(sx / 2., sy / 2., sz / 2.);
+  auto   modMat = desc.material(mod.attr<std::string>(_Unicode(material)));
+  Volume modVol("module_vol", modShape, modMat);
+  modVol.setSensitiveDetector(sens);
+  modVol.setVisAttributes(desc.visAttributes(mod.attr<std::string>(_Unicode(vis))));
+
+  // no wrapper
+  if (!plm.hasChild(_Unicode(wrapper))) {
+    return std::make_tuple(modVol, Position{sx, sy, sz});
+    // build wrapper
+  } else {
+    auto wrp       = plm.child(_Unicode(wrapper));
+    auto thickness = wrp.attr<double>(_Unicode(thickness));
+    if (thickness < 1e-12 * mm) {
+      return std::make_tuple(modVol, Position{sx, sy, sz});
+    }
+    auto   wrpMat = desc.material(wrp.attr<std::string>(_Unicode(material)));
+    Box    wrpShape((sx + thickness) / 2., (sy + thickness) / 2., sz / 2.);
+    Volume wrpVol("wrapper_vol", wrpShape, wrpMat);
+    wrpVol.placeVolume(modVol, Position(0., 0., 0.));
+    wrpVol.setVisAttributes(desc.visAttributes(wrp.attr<std::string>(_Unicode(vis))));
+    return std::make_tuple(wrpVol, Position{sx + thickness, sy + thickness, sz});
+  }
+}
+
+// place modules, id must be provided
+static std::tuple<int, int> add_individuals(Detector& desc, Assembly& env, xml::Collection_t& plm,
+                                            SensitiveDetector& sens, int sid)
+{
+  auto [modVol, modSize] = build_module(desc, plm, sens);
+  int sector_id          = dd4hep::getAttrOrDefault<int>(plm, _Unicode(sector), sid);
+  int nmodules           = 0;
+  for (xml::Collection_t pl(plm, _Unicode(placement)); pl; ++pl) {
+    Position    pos(dd4hep::getAttrOrDefault<double>(pl, _Unicode(x), 0.),
+                    dd4hep::getAttrOrDefault<double>(pl, _Unicode(y), 0.),
+                    dd4hep::getAttrOrDefault<double>(pl, _Unicode(z), 0.));
+    Position    rot(dd4hep::getAttrOrDefault<double>(pl, _Unicode(rotx), 0.),
+                    dd4hep::getAttrOrDefault<double>(pl, _Unicode(roty), 0.),
+                    dd4hep::getAttrOrDefault<double>(pl, _Unicode(rotz), 0.));
+    auto        mid   = pl.attr<int>(_Unicode(id));
+    Transform3D tr    = Translation3D(pos.x(), pos.y(), pos.z()) * RotationZYX(rot.z(), rot.y(), rot.x());
+    auto        modPV = env.placeVolume(modVol, tr);
+    modPV.addPhysVolID("sector", sector_id).addPhysVolID("module", mid);
+    nmodules++;
+  }
+
+  return {sector_id, nmodules};
+}
 DECLARE_DETELEMENT(B0_ECAL, createDetector)
