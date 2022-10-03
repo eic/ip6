933bc88f1beb41ca9d4d1ad9123c848af0d31784
Sakib Rahman <rahmans@myumanitoba.ca>
2022-10-02 20:04:39 -0500

    If not specified, set the envelope clearance parameter for B0 ecal to 0
    If not specified, set the envelope clearance parameter for B0 ecal to 0. Reasonable default assumption.

--- src/B0ECal_geo.cpp
+++ src/B0ECal_geo.cpp
@@ -148,7 +148,7 @@ static std::tuple<int, int> add_disk(Detector& desc, Assembly& env, xml::Collect
   int    id_begin        = dd4hep::getAttrOrDefault<int>(plm, _Unicode(id_begin), 1);
   double rmin            = plm.attr<double>(_Unicode(rmin));
   double rintermediate   = plm.attr<double>(_Unicode(rintermediate));
-  double envelopeclearance = plm.attr<double>(_Unicode(envelopeclearance));
+  double envelopeclearance = dd4hep::getAttrOrDefault<int>(plm, _Unicode(envelopeclearance), 0);
   double rmax            = plm.attr<double>(_Unicode(rmax));
   double phimin          = dd4hep::getAttrOrDefault<double>(plm, _Unicode(phimin), 0.);
   double phimax          = dd4hep::getAttrOrDefault<double>(plm, _Unicode(phimax), 2. * M_PI);
