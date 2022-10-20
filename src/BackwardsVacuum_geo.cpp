e44402052d42e17249d020f73e93e90cd382c40d
simonge <simongardner1@hotmail.com>
2022-10-20 20:55:44 +0100

    Fised overlaps but removed some functionality

--- src/BackwardsVacuum_geo.cpp
+++ src/BackwardsVacuum_geo.cpp
@@ -14,28 +14,35 @@ using namespace dd4hep;
 
 static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/)
 {
+  
   xml_det_t x_det   = e;
   string    detName = x_det.nameStr();
   int       detID   = x_det.id();
 
+  // Dimensions of main beamline pipe
   xml::Component  dim = x_det.child(_Unicode(dimensions));
   double    WidthL    = dim.attr<double>(_Unicode(xL));
-  double    WidthR    = dim.attr<double>(_Unicode(xL));
+  double    WidthR    = dim.attr<double>(_Unicode(xR));
 
   double    Width     = (WidthL+WidthR)/2;
   double    Height    = dim.y();
   double    Thickness = dim.z();
 
-  //Materials
+  // Materials
   Material Vacuum  = desc.material("Vacuum");
   Material Steel   = desc.material("StainlessSteel");
-
+  
+  // Central focal point of the geometry
   xml::Component pos = x_det.child(_Unicode(focus));
-  xml_dim_t rot  = x_det.rotation();
   double    off  = pos.z();
+
+  // Beamline rotation
+  xml_dim_t rot  = x_det.rotation();
+
+  // Beampipe thickness
   double    wall = dd4hep::getAttrOrDefault(x_det, _Unicode(wall), 1*mm);
 
-  //Make bounding box to make IntersectionSolid with other components
+  // Make bounding box to make IntersectionSolid with other components
   xml::Component BB = x_det.child(_Unicode(bounding));
   double    BB_MinX = BB.xmin();
   double    BB_MinY = BB.ymin();
@@ -50,24 +57,27 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/
 
   Box Far_Backwards_Box(BB_X,BB_Y,BB_Z);
 
-  //Entry box description
+  // Entry box geometry description joining magnet, taggers and lumi
   xml::Component EB = x_det.child(_Unicode(exitdim));
-  double ED_X = EB.x();
-  double ED_Y = EB.y();
-  double ED_Z = off-EB.attr<double>(_Unicode(lumiZ));
-  double Lumi_R = EB.attr<double>(_Unicode(lumiR));
-  double exitTheta = EB.attr<double>(_Unicode(maxTheta));
+  double ED_X       = EB.x();
+  double ED_Y       = EB.y();
+  double ED_Z       = off-EB.attr<double>(_Unicode(lumiZ));
+  double Lumi_R     = EB.attr<double>(_Unicode(lumiR));
+
+  // Maximum theta to exit the dipole from 
+  double exitTheta  = EB.attr<double>(_Unicode(maxTheta));
 
   // Generic box for making intersection solid with
   double xbox = 10*m;
   double ybox = 10*m;
   double zbox = 50*m;
+
   Box Cut_Box(xbox,ybox,zbox);
 
-  //Central pipe box
+  // Central pipe box
   Box Extended_Beam_Box(Width+wall,Height+wall,Thickness);
 
-  //Central vacuum box 
+  // Central vacuum box 
   Box Extended_Vacuum_Box(Width,Height,Thickness);
 
   Solid Wall_Box   = Extended_Beam_Box;
@@ -79,63 +89,72 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/
   DetElement det(detName, detID);
 
   //-----------------------------------------------------------------
-  //Add Tagger box containers and vacuum box extension for modules
+  // Add Tagger box containers and vacuum box extension for modules
   //-----------------------------------------------------------------
   for (xml_coll_t mod(x_det, _Unicode(module)); mod; ++mod ) {
-
+    
     int    moduleID  = dd4hep::getAttrOrDefault(mod, _Unicode(id), 0);
     string moduleName= dd4hep::getAttrOrDefault(mod, _Unicode(modname), "Tagger0");
+
+    // Offset from the electron beam
     double tagoff    = dd4hep::getAttrOrDefault(mod, _Unicode(offset_min), 50.0*mm    );
+
+    // Theta coverage expected
     double thetamin  = dd4hep::getAttrOrDefault(mod, _Unicode(theta_min),  0.030*rad)-rot.theta();
     double thetamax  = dd4hep::getAttrOrDefault(mod, _Unicode(theta_max),  0.030*rad)-rot.theta();
+
+    // Align box to max or minimum theta expected at the tagger from focal point
     bool   max_align = dd4hep::getAttrOrDefault(mod, _Unicode(max_align),  false    );
+
+    // Extend the beam vacuum around the taggers
     bool   extend_vacuum = dd4hep::getAttrOrDefault(mod, _Unicode(extend_vacuum),  true    );
 
+    // Size f the actual tagger box, replicated in BackwardsTagger
     xml_dim_t moddim = mod.child(_Unicode(dimensions));
     double w         = moddim.x();
     double h         = moddim.y();
     double tagboxL   = moddim.z();
     
-//     auto tag_w = w;
-//     auto tag_h = h;
-    auto vac_w = w+wall/2;
+    // Width and height of vacuum volume 
+    auto vac_w = w;
+    //    auto vac_w = w+wall/2; // Adds space for wall to be added in tagger geometry
     auto vac_h = h;
-//     auto vac_w = w-wall/2;
-//     auto vac_h = h-wall;
-    w+=wall;
-    h+=wall;
-     
-    auto theta      = thetamin+0.0001*rad;
-    auto thetaA     = thetamin; //HACK
-    auto offsetx    = -w*(cos(theta));
-    auto offsetz    = w*(sin(theta));
+
+    // Width and height of box volume
+    auto box_w = w+wall;
+    auto box_h = h+wall;
+   
+    auto theta      = thetamin;
+    auto offsetx    = -(box_w-wall)*(cos(theta));
+    auto offsetz    = (box_w-wall)*(sin(theta));
     auto vacoffsetx = -vac_w*(cos(theta));
     auto vacoffsetz = vac_w*(sin(theta));
     auto l          = (tagoff)/(sin(theta));
+    //    auto l          = (tagoff)/(sin(theta));
 
-    auto tagoffsetx = vacoffsetx-(l+tagboxL/2)*sin(thetaA);
-    auto tagoffsetz = vacoffsetz-(l+tagboxL/2)*cos(thetaA);
+    auto tagoffsetx = vacoffsetx-(l+tagboxL)*sin(theta);
+    auto tagoffsetz = vacoffsetz-(l+tagboxL)*cos(theta);
+//     auto tagoffsetx = vacoffsetx-(l+tagboxL/2)*sin(theta);
+//     auto tagoffsetz = vacoffsetz-(l+tagboxL/2)*cos(theta);
 
     if(max_align){
-      theta  = thetamax+0.0001*rad;
-      thetaA = thetamax;
-      offsetx = -offsetx;
-      offsetz    = -offsetz;
-      vacoffsetx = (vac_w+wall)*(cos(theta));
-      vacoffsetz = -(vac_w+wall)*(sin(theta));
+      theta      = thetamax;
+      offsetx    = (box_w-wall)*(cos(theta));
+      offsetz    = -(box_w-wall)*(sin(theta));
+      vacoffsetx = vac_w*(cos(theta));
+      vacoffsetz = -vac_w*(sin(theta));
       l      = (2*offsetx+tagoff)/sin(theta);
-      tagoffsetx = -wall+vacoffsetx-(l+tagboxL/2)*sin(thetaA);
-      tagoffsetz = vacoffsetz-(l+tagboxL/2)*cos(thetaA);
+      tagoffsetx = vacoffsetx-(l+tagboxL)*sin(theta);
+      tagoffsetz = vacoffsetz-(l+tagboxL)*cos(theta);
+//       tagoffsetx = -wall+vacoffsetx-(l+tagboxL/2)*sin(theta);
+//       tagoffsetz = vacoffsetz-(l+tagboxL/2)*cos(theta);
     }
 
 
-    std::cout << thetamin << " "  << thetamax << std::endl; 
-    std::cout << max_align << " " << w << " "  << h << " "  << l << " "  << theta << std::endl; 
-
-    Box TagWallBox(w,h,l+tagboxL);
+    Box TagWallBox(box_w,box_h,l+tagboxL);
     Box TagVacBox(vac_w,vac_h,l+tagboxL);
 
-    RotationZYX rotate(0,thetaA,0);
+    RotationY rotate(theta);
 
     Volume   mother    = DetAssemblyAir;
     
@@ -147,7 +166,7 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/
     
     Assembly TaggerAssembly("tagAssembly");
     
-    PlacedVolume pv_mod2 = mother.placeVolume(TaggerAssembly, Transform3D(rotate, Position(tagoffsetx,0,tagoffsetz)));//Very strange y offset needs correcting for...
+    PlacedVolume pv_mod2 = mother.placeVolume(TaggerAssembly, Transform3D(rotate, Position(tagoffsetx,0,tagoffsetz)));//Very strange y is not centered and offset needs correcting for...
     pv_mod2.addPhysVolID("module", moduleID);
     DetElement moddet(moduleName, moduleID);
     moddet.setPlacement(pv_mod2);
@@ -155,10 +174,16 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/
     
   }
 
+  //-----------------------------------------------------------------
+  // Cut off any vacuum right of the main beamline 
+  //-----------------------------------------------------------------
+
   Wall_Box   = IntersectionSolid(Wall_Box,   Cut_Box,   Position(-xbox+Width+wall,0,0));
   Vacuum_Box = IntersectionSolid(Vacuum_Box, Cut_Box,   Position(-xbox+Width,0,0));
 
+  //-----------------------------------------------------------------
   // Luminosity connecting box 
+  //-----------------------------------------------------------------
   bool addLumi = dd4hep::getAttrOrDefault(x_det, _Unicode(lumi),  true    );
 
   if(addLumi){
@@ -174,48 +199,46 @@ static Ref_t create_detector(Detector& desc, xml_h e, SensitiveDetector /*sens*/
 
   }
   
+
+  //-----------------------------------------------------------------
+  // Restrict tagger boxes into region defined by exitTheta from the dipole magnet
+  //-----------------------------------------------------------------
   double exitDist = BB_MinZ-off;
-  // Restrict tagger boxes into region right, connected to beampipe magnet exit
-  double cutX = (ED_X-exitDist*tan(-rot.theta()))*cos(rot.theta());
-  double cutZ = (ED_X-exitDist*tan(-rot.theta()))*sin(rot.theta())+exitDist*cos(rot.theta());
-    
+  double cutX     = (ED_X-exitDist*tan(-rot.theta()))*cos(rot.theta());
+  double cutZ     = (ED_X-exitDist*tan(-rot.theta()))*sin(rot.theta())+exitDist*cos(rot.theta());
+  double cutXwall = (ED_X-wall-exitDist*tan(-rot.theta()))*cos(rot.theta());
+  double cutZwall = (ED_X-wall-exitDist*tan(-rot.theta()))*sin(rot.theta())+exitDist*cos(rot.theta());
 
   Wall_Box   = IntersectionSolid(Wall_Box,   Cut_Box,   Transform3D(RotationY(exitTheta),Position(xbox-cutX,0,cutZ)));
-  Vacuum_Box = IntersectionSolid(Vacuum_Box, Cut_Box,   Transform3D(RotationY(exitTheta),Position(xbox-cutX,0,cutZ)));
+  Vacuum_Box = IntersectionSolid(Vacuum_Box, Cut_Box,   Transform3D(RotationY(exitTheta),Position(xbox-cutXwall,0,cutZwall)));
 
-  // Cut pipe solids so they are only in the far backwards box 
+
+  //-----------------------------------------------------------------
+  // Cut solids so they are only in the far backwards box 
+  //-----------------------------------------------------------------
   RotationY   rotate2(-rot.theta());
-  //Position    position(0,0,(off-BB_Z)/cos(rot.theta()));
   Position    position(0,0,(exitDist-BB_Z)/cos(rot.theta()));
 
   IntersectionSolid Wall_Box_Sub  (Wall_Box,   Far_Backwards_Box, Transform3D(rotate2, position));
   IntersectionSolid Vacuum_Box_Sub(Vacuum_Box, Far_Backwards_Box, Transform3D(rotate2, position));
-  
 
   Volume vacVol("Vacuum_Box", Vacuum_Box_Sub, Vacuum);
-  vacVol.setVisAttributes(desc.visAttributes("YellowVis"));
+  vacVol.setVisAttributes(desc.visAttributes("RedGreenVis"));
   vacVol.placeVolume(DetAssembly); 
   Volume wallVol("Tagger_Box", Wall_Box_Sub, Steel);
+  wallVol.setVisAttributes(desc.visAttributes("RedVis"));
   wallVol.placeVolume(vacVol);
 
   Assembly backAssembly("assembly");
   backAssembly.placeVolume(wallVol);
   backAssembly.placeVolume(DetAssemblyAir);
 
-
   // placement in mother volume
   Transform3D  tr(RotationY(rot.theta()), Position(pos.x(), pos.y(), pos.z()));
   PlacedVolume detPV = desc.pickMotherVolume(det).placeVolume(backAssembly, tr);
-  //PlacedVolume detPV = desc.pickMotherVolume(det).placeVolume(wallVol, tr);
   detPV.addPhysVolID("system", detID);
   det.setPlacement(detPV);
  
-  for (xml_coll_t mod(x_det, _Unicode(module)); mod; ++mod ) {
-
-    string moduleName= dd4hep::getAttrOrDefault(mod, _Unicode(modname), "Tagger0");
-    desc.declareParent(moduleName, det);
-  }
-
   return det;
 }
 
